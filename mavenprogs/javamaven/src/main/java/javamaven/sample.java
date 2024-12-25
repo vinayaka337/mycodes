8da72effa52c928cbcfef085f3eaf81c0631c33a
Vinayaka337 <sudhakarreddy9885@github.com>
2024-12-25 20:54:20 +0530

    added maven project

---
+++ mavenprogs/javamaven/src/main/java/javamaven/sample.java
@@ -0,0 +1,10 @@
+package javamaven;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("This is from maven project java program");
+	}
+
+}
